e7de306d47fe389dacbeb0b3f8e299b90ba4e371
maziz07 <mazizyr@gmail.com>
2024-06-11 08:29:50 +1000

    new code added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ function App() {
 
   return (
    <div className='flex flex-col'>
-    <section>
+    <section className='min-h-screen flex flex-col'>
       <header>
-
+        <h1>Scribe<span className='text-blue-400'>It</span></h1>
+        <button>
+          <p>New</p>
+        </button>
       </header>
-      <main>
+      <main className='flex-1'>
         
       </main>
     </section>
       <h1 className='text-green-400'>
-      Hello world!
+      Hello
     </h1>
    </div>
   )
