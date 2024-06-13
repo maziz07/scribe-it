e4b758f271025c865d8d892074c3533b5b31142f
maziz07 <mazizyr@gmail.com>
2024-06-13 18:10:23 +1000

    updated header and homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
   return (
     <header className='flex items-center justify-between gap-4 p-4'>
-        <h1>Scribe<span className='text-blue-400'>It</span></h1>
+        <h1 className='font-medium'>Scribe<span className='text-blue-400 bold'>It</span></h1>
         <button className='flex items-center gap-2'>
           <p>New</p>
           <i className="fa-solid fa-plus"></i>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,23 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <main className='flex-1 p-4 flex flex-col justify-center'>
-      adasdasdad  
+    <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 
+    justify-center pb-10'>
+      <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>
+        Scribe<span className='text-blue-400 bold'>It</span></h1>
+      <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;
+        </span> Transcribe <span className='text-blue-400'>&rarr;
+        </span> Translate</h3>  
+        <button className='flex items-center text-base justify-between
+        gap-4 mx-auto w-72 max-w-full my-4'>
+            <p>Record</p>
+            <i className="fa-solid fa-microphone"></i>
+        </button>
+        <p className='text-base'>Or <label className='text-blue-400 cursor pointer
+        hover:text-blue-600 duration-200'>upload <input 
+        className='hidden' type='file' accept='.mp3,.wave'/>
+        </label> a mp3 file</p>
+        <p className='italic text-slate-500'>Free now Free forever</p>
     </main>
   )
 }
