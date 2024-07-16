bffc4792ba88cb72176db1d7f31985cbf2ae569e
maziz07 <mazizyr@gmail.com>
2024-07-16 22:42:00 +1000

    updated index and components

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Information() {
   return (
-    <div>Information</div>
+    <main className='flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 md:gap-5 
+    justify-center pb-20 w-72 sm:w-96 max-w-full mx-auto'>
+        <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl'>
+        Your<span className='text-blue-400 bold'>File</span></h1>
+    </main>
   )
 }
