47e02df35ebc89f705ba90b2951def36686b565a
maziz07 <mazizyr@gmail.com>
2024-07-24 20:14:19 +1000

    new component created

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -8,13 +8,15 @@ export default function Information() {
         <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl whitespace-nowrap'>
         Your<span className='text-blue-400 bold'>Transcription</span></h1>
 
-        <div className='flex mx-auto bg-white shadow rounded-full overflow-hiddent
-        items-center gap-2'>
-          <button className={'px-4 py-1 font-medium' + 
+        <div className='grid grid-cols-2 mx-auto bg-white shadow rounded-full overflow-hiddent
+        items-center'>
+          <button onClick={() => setTab
+            ('transcription')} className={'px-4 duration-200 py-1 font-medium' + 
           (tab === 'transcription' ? ' bg-blue-400 text-white'
              : ' text-blue-400 hover:text-blue-600 ')}
           >Transcription</button>
-          <button className={'px-4 py-1 font-medium' +
+          <button onClick={() => setTab
+            ('translation')} className={'px-4 duration-200 py-1 font-medium' +
            (tab === 'transcription' ? ' bg-blue-400 text-white'
              : ' text-blue-400 hover:text-blue-600 ')}>Translation</button>
         </div>
