20f8617e6eca081bc526f1603414c3503bf791dc
maziz07 <mazizyr@gmail.com>
2025-01-16 23:35:05 +1100

    updated new scripts

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Information from './components/Information'
 import Transcribing from './components/Transcribing'
 import { MessageTypes } from './utils/presets'
 
-
 function App() {
   const [file, setFile] = useState(null)
   const [audioStream, setAudioStream] = useState(null)
@@ -17,88 +16,84 @@ function App() {
 
   const isAudioAvailable = file || audioStream
 
-  function handleAudioReset(){
+  function handleAudioReset() {
     setFile(null)
     setAudioStream(null)
   }
 
   const worker = useRef(null)
+
   useEffect(() => {
     if (!worker.current) {
-      worker.current = new Worker(new URL('./utils/whisper.worker.js', import.meta.url),
-    {type: 'module'
-
-    })
+      worker.current = new Worker(new URL('./utils/whisper.worker.js', import.meta.url), {
+        type: 'module'
+      })
     }
+
     const onMessageReceived = async (e) => {
       switch (e.data.type) {
         case 'DOWNLOADING':
           setDownloading(true)
           console.log('DOWNLOADING')
           break;
-          case 'LOADING':
+        case 'LOADING':
           setLoading(true)
           console.log('LOADING')
           break;
-          case 'RESULT':
-            setOutput(e.data.results)
+        case 'RESULT':
+          setOutput(e.data.results)
+          console.log(e.data.results)
           break;
-          case 'INFERENCE_DONE':
-            setFinished(true)
-            console.log("DONE")
+        case 'INFERENCE_DONE':
+          setFinished(true)
+          console.log("DONE")
           break;
       }
-
     }
 
-    worker.current.addEventListener('message',
-      onMessageReceived)
-    
-    return () => worker.current.removeEventListener
-    ('message', onMessageReceived)
+    worker.current.addEventListener('message', onMessageReceived)
 
-  }, [] )
+    return () => worker.current.removeEventListener('message', onMessageReceived)
+  })
 
-    async function readAudioFrom(file) {
-      const sampling_rate = 16000
-      const audioCTX = new AudioContext({sampleRate: sampling_rate})
-      const response = await file.arrayBuffer()
-      const decoded = await audioCTX.decodeAudioData(response)
-      const audio = decoded.getChannelData(0)
-      return audio
-    }
+  async function readAudioFrom(file) {
+    const sampling_rate = 16000
+    const audioCTX = new AudioContext({ sampleRate: sampling_rate })
+    const response = await file.arrayBuffer()
+    const decoded = await audioCTX.decodeAudioData(response)
+    const audio = decoded.getChannelData(0)
+    return audio
+  }
 
-    async function handleFormSubmission() {
-      if (!file && !audioStream) {return}
+  async function handleFormSubmission() {
+    if (!file && !audioStream) { return }
 
-      let audio = await readAudioFrom(file ? file :audioStream)
-      const model_name = `openai/whisper-tiny.en`
+    let audio = await readAudioFrom(file ? file : audioStream)
+    const model_name = `openai/whisper-tiny.en`
 
-      worker.current.postMessage({
-        type: MessageTypes.INFERENCE_REQUEST ,
-        audio,
-        model_name
-      })
-    }
+    worker.current.postMessage({
+      type: MessageTypes.INFERENCE_REQUEST,
+      audio,
+      model_name
+    })
+  }
 
   return (
-   <div className='flex flex-col max-w-[1000p] mx-auto w-full'>
-    <section className='min-h-screen flex flex-col'>
-      <Header />
-      {output ? (
-        <Information/>
-      ) : loading ? (
-        <Transcribing/>
-     
-      ): isAudioAvailable? (
-        <FileDisplay handleAudioReset={handleAudioReset} 
-        file={file} audioStream={setAudioStream} />
-      ) : (
-      <HomePage setFile={setFile} setAudioStream={setAudioStream} />
-      )}
-    </section>
+    <div className='flex flex-col max-w-[1000px] mx-auto w-full'>
+      <section className='min-h-screen flex flex-col'>
+        <Header />
+        {output ? (
+          <Information output={output} finished={finished}/>
+        ) : loading ? (
+          <Transcribing />
+        ) : isAudioAvailable ? (
+          <FileDisplay handleFormSubmission={handleFormSubmission} handleAudioReset={handleAudioReset} file={file} audioStream={audioStream} />
+        ) : (
+          <HomePage setFile={setFile} setAudioStream={setAudioStream} />
+        )}
+      </section>
       <footer></footer>
-   </div>
+    </div>
   )
 }
 
